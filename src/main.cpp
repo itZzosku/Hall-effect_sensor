f15f0c4962aa5211acb59cd39930b1273c97c2ff
Osku Väyrynen <osku27@hotmail.com>
2020-11-25 04:38:54 +0200

    Updated the handling of the library and added a simple counter.

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 #include <TM1638.h>
 
-// define a module on data pin 12, clock pin 13 and strobe pin 11 
+// define a module on data pin 12, clock pin 13 and strobe pin 11
 TM1638 module(12, 13, 11, true, 1);
 
 byte buttons;
@@ -12,41 +12,47 @@ byte buttons;
 int Players = 1;
 int Cards = 5;
 
+unsigned long Time;
 
-
-void setup() {
+void setup()
+{
 
   Serial.begin(9600);
 }
 
-void loop() {
-   // display a variables at 1st and 5th position
+void loop()
+{
+
+  Time = millis();
+  // display a variables at 1st and 5th position
   module.setDisplayDigit(Players, 0, false);
   module.setDisplayDigit(Cards, 4, false);
- 
-  buttons = module.getButtons();
-  
-  Serial.print (Players);
-  Serial.print (",");
-  Serial.println (Cards);
-  delay(200); 
 
-  if (buttons != 0) 
+  buttons = module.getButtons();
+
+  Serial.print(Time);
+  Serial.print(",");
+  Serial.print(Players);
+  Serial.print(",");
+  Serial.println(Cards);
+  delay(100);
+
+  if (buttons != 0)
   {
-    delay(300); 
-                            
+    delay(300);
+
     switch (buttons)
-      {
-        case 1:
-          Players = ++Players;
-          Cards = Cards+Players;
-          buttons = 0;
-          break;
-        case 2:
-          Players = --Players;
-          Cards = Cards-Players;
-          buttons = 0;
-          break;                 
-     }
+    {
+    case 1:
+      Players = Players + 1;
+      Cards = Cards + Players;
+      buttons = 0;
+      break;
+    case 2:
+      Players = Players - 1;
+      Cards = Cards - Players;
+      buttons = 0;
+      break;
     }
+  }
 }
